1a66b045fbd45791b70c1fb5d4f08d41562fc902
Łukasz Koziara <SirLacky@users.noreply.github.com>
2019-05-19 10:02:22 +0200

    Added AdminDTO

---
+++ src/main/java/com/github/sirlacky/GiveBack/dtos/AdminDTO.java
@@ -0,0 +1,14 @@
+package com.github.sirlacky.GiveBack.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminDTO {
+
+    private String username;
+
+}
